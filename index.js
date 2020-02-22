17504b830bc3d40fd431f850f9bf5e79ea45aab8
heckman-codes <liamheckmancode@gmail.com>
2020-02-21 16:33:50 -0800

    dadbot

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+    console.log(msg.content)
+    console.log(msg.content.indexOf("I'm"));
+    if (msg.content.includes("I'm")) {
+        if (msg.author.bot) return
+        var message = msg.content.replace("I'm", '');
+        var newMessage = "Hi," + message + ", I'm Dad!"
+        msg.reply(newMessage);
+    }
+
+});
